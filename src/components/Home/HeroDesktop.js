c342ca6537a89746a192c47dabf85b00eb529e93
hitesh-bhardwaj <70434060+hitesh-bhardwaj@users.noreply.github.com>
2023-10-11 12:26:18 +0530

    Preloader Update

--- src/components/Home/HeroDesktop.js
+++ src/components/Home/HeroDesktop.js
@@ -38,8 +38,15 @@ const HeroDesktop = () => {
           opacity: 1,
           y: 0,
           stagger: 0.1
-        }
-      );
+        }).fromTo(".reel-anim", {
+          scale: 0,
+          opacity: 0,
+        },{
+          scale: 1,
+          opacity: 1,
+          delay: -1.8,
+          duration: 1.3,
+        });
     return () => tl.kill();
   }, []);
 
@@ -82,15 +89,7 @@ const HeroDesktop = () => {
                   </div>
 
                   <div className="reel-hero">
-                    <motion.div
-                      initial={{ opacity: 0, scale: 0 }}
-                      animate={{ opacity: 1, scale: 1 }}
-                      transition={{
-                        ease: "easeInOut",
-                        duration: 1,
-                        delay: 6.5,
-                      }}
-                    >
+                    <div className='reel-anim'>
                       <video
                         src="/assets/reels/hero.webm"
                         muted
@@ -99,7 +98,7 @@ const HeroDesktop = () => {
                         preload="auto"
                         className="hero-video"
                       ></video>
-                    </motion.div>
+                    </div>
                   </div>
                 </div>
 
--- src/components/PreLoader/PreLoader.js
+++ src/components/PreLoader/PreLoader.js
@@ -101,9 +101,6 @@ export default function Index() {
           <span className={styles.mainLoaderBar} id='loaderbars'></span>
           <span className={styles.mainLoaderBar} id='loaderbars'></span>
         </div>
-        {/* <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
-            <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z" />
-        </svg> */}
       </>
     </div>
   );
