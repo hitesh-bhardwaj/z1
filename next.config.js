a41c090680632ca7289918c8a76187a4aae9e599
hitesh-bhardwaj <70434060+hitesh-bhardwaj@users.noreply.github.com>
2023-11-02 16:12:45 +0530

    update server file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  compress: true,
   reactStrictMode: true,
   images: {
     domains: ['z1'],
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const next = require('next');
+const compression = require('compression');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Use the compression middleware to enable Gzip compression
+  server.use(compression());
+
+  // Handle Next.js pages and API routes
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
