573156561b016ce5978f3587651b72e676a2b9e1
hitesh-bhardwaj <70434060+hitesh-bhardwaj@users.noreply.github.com>
2023-11-16 12:26:20 +0530

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,35 +20,27 @@ export default function App({ Component, pageProps }) {
 
       <Analytics />
       <Script
-        strategy="afterInteractive"
-        id="ms-clarity"
-        dangerouslySetInnerHTML={{
-          __html: `
-                    (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "josmn10ai7");
-        `,
-        }}
-      />
+        id="ms-clarity">
+        { `
+          (function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "josmn10ai7");
+        `}
+      </Script>
     
       <Script
-        strategy="afterInteractive"
         id="google-tag-manager"
         src="https://www.googletagmanager.com/gtag/js?id=G-PKJE9LVB35"
       />
 
       <Script
-        strategy="afterInteractive"
-        id="google-analytics"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-7C8S5767C3', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      />
+        id="google-analytics">
+          {` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7C8S5767C3', {
+              page_path: window.location.pathname,
+            });
+          `}
+      </Script>
     </>
   );
 }
