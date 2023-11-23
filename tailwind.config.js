558191db391189812958fdacf0a526f8b067a224
hitesh-bhardwaj <70434060+hitesh-bhardwaj@users.noreply.github.com>
2023-11-23 17:46:19 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
         '3xl': '0 10px 30px -10px rgba(0, 0, 0, 0.2)',
       },
       screens: {
-        '2xl': '1440px',
+        '2xl': '1280px',
         '3xl': '1680px',
       },
     },
